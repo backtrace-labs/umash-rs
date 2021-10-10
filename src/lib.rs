a5d848f68a3fad5850b4ee6cb6bd9e62697ea3c1
Paul Khuong <pvk@pvk.ca>
2021-10-09 21:26:36 -0400

    docs: more xref links

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! UMASH defines an almost-universal family of hash functions.  Each
-//! `Params` struct defines a specific hash function; when the
+//! [`Params`] struct defines a specific hash function; when the
 //! parameters are generated pseudorandomly the probability that two
 //! different inputs of up to `s` bytes collide is at most `ceil(s /
 //! 4096) 2^-55` for the 64-bit hash.  The 128-bit fingerprint reduces
@@ -11,64 +11,67 @@
 use std::marker::PhantomData;
 use umash_sys as ffi;
 
-/// A `Params` stores a set of hashing parameters.
+/// A [`Params`] stores a set of hashing parameters.
 ///
-/// By default, each `Params` is generated independently with unique
+/// By default, each [`Params`] is generated independently with unique
 /// pseudorandom parameters.  Call `Params::derive` to generate
 /// repeatable parameters that will compute the same hash and
 /// fingerprint values across processes, programs, and architectures.
 ///
-/// This struct consists of 38 `u64` parameters, so while `Params` do
-/// not own any resource, they should be passed by reference rather
+/// This struct consists of 38 `u64` parameters, so while [`Params`]
+/// do not own any resource, they should be passed by reference rather
 /// than copied, as much as possible.
 ///
-/// `Params` implements `std::hash::BuildHasher`; pass a `&'static
-/// Params` to, e.g., a `std::collections::HashMap`, and the hash
-/// values will be computed with as the primary UMASH value for these
-/// `Params`, and `seed = 0`.
+/// [`Params`] implements [`std::hash::BuildHasher`]: pass a `&'static
+/// Params` to, e.g., [`std::collections::HashMap::with_hasher`], and
+/// the hash values will be computed with as the primary UMASH value
+/// for these [`Params`], and `seed = 0`.
 #[derive(Clone)]
 pub struct Params(ffi::umash_params);
 
-/// A given `Params` struct defines a pair of 64-bit hash functions.
-/// The `Hash` is the primary hash value; we find a 128-bit
-/// fingerprint by combining that primary value with the `Secondary`
-/// hash (in practice, it's more efficient to compute both hash values
-/// concurrently, when one knows they want a fingerprint).
+/// A given [`Params`] struct defines a pair of 64-bit hash functions.
+/// The [`UmashComponent::Hash`] is the primary hash value; we find a
+/// 128-bit fingerprint by combining that primary value with the
+/// [`UmashComponent::Secondary`] hash (in practice, it's more
+/// efficient to compute both hash values concurrently, when one knows
+/// they want a fingerprint).
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum UmashComponent {
     /// Compute the primary 64 bit hash (the first `u64` in a
-    /// `Fingerprint`)
+    /// [`Fingerprint`])
     Hash = 0,
 
     /// Compute the secondary 64-bit value (the second `u64` in a
-    /// `Fingerprint`).  This secondary value is slightly less
-    /// well distributed and collision resistant than the primary,
-    /// but combines efficiently with the primary to compute a
-    /// reasonably strong 128-bit Fingerprint.
+    /// [`Fingerprint`]).  This secondary value is slightly less well
+    /// distributed and collision resistant than the primary, but
+    /// combines efficiently with the primary to compute a reasonably
+    /// strong 128-bit [`Fingerprint`].
     Secondary = 1,
 }
 
-/// A 128-bit `Fingerprint` is constructed by UMASH as if we had
-/// computed the `Hash` and `Secondary` functions, and stored them
-/// in the `hash` array in order.
+/// A 128-bit [`Fingerprint`] is constructed by UMASH as if we had
+/// computed the [`UmashComponent::Hash`] and
+/// [`UmashComponent::Secondary`] functions, and stored them in the
+/// [`Fingerprint::hash`] array in order.
 ///
-/// This makes it possible to check a document against a `Fingerprint`
-/// by only computing the 64-bit `Hash`, and comparing that with
-/// `hash[0]`: the comparison gives us less confidence, but is faster
-/// to compute.
+/// This makes it possible to check a document against a
+/// [`Fingerprint`] by only computing the 64-bit
+/// [`UmashComponent::Hash`], and comparing that with
+/// `Fingerprint::hash[0]`: the comparison gives us less confidence,
+/// but is faster to compute.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub struct Fingerprint {
     pub hash: [u64; 2],
 }
 
 impl Fingerprint {
-    /// Returns the `UmashComponent::Hash` component of the fingerprint.
+    /// Returns the [`UmashComponent::Hash` component of the fingerprint.
     #[inline(always)]
     pub fn hash(&self) -> u64 {
         self.hash[0]
     }
 
-    /// Returns the `UmashComponent::Secondary` component of the
+    /// Returns the [`UmashComponent::Secondary`] component of the
     /// fingerprint.
     #[inline(always)]
     pub fn secondary(&self) -> u64 {
@@ -81,48 +84,51 @@ impl Fingerprint {
     }
 }
 
-/// A `Hasher` implements one of the two hash 64-bit functions defined
-/// by a specific `Params` struct, further tweaked by a seed.
-/// Construct `Hasher`s with `Params::hasher`,
-/// `Params::secondary_hasher`, or `Params::component_hasher`.
+/// A [`Hasher`] implements one of the two hash 64-bit functions
+/// defined by a specific [`Params`] struct, further tweaked by a
+/// seed.  Construct [`Hasher`]s with [`Params::hasher`],
+/// [`Params::secondary_hasher`], or [`Params::component_hasher`].
 ///
-/// The hash value is a function of the `Params`, the
-/// `UmashComponent`, the seed, and of the bytes written to the
-/// `Hasher`, but independent of the size of the individual slices
+/// The hash value is a function of the [`Params`], the
+/// [`UmashComponent`], the seed, and of the bytes written to the
+/// [`Hasher`], but independent of the size of the individual slices
 /// written to the hasher.
 ///
 /// In other words, it doesn't matter how we partition an input, the
-/// `Hasher` computes the same hash value as a one-shot UMASH call
+/// [`Hasher`] computes the same hash value as a one-shot UMASH call
 /// for the parameters and the concatenated input bytes.
 #[derive(Clone)]
 pub struct Hasher<'params>(ffi::umash_state, PhantomData<&'params Params>);
 
-/// A `Fingerprinter` implements the 128-bit fingerprinting function
-/// defined by a specific `Params` struct, further tweaked by a seed.
-/// Construct `Fingerprinter`s with `Params::fingerprinter`.
+/// A [`Fingerprinter` implements the 128-bit fingerprinting function
+/// defined by a specific [`Params`] struct, further tweaked by a seed.
+/// Construct [`Fingerprinter`]s with [`Params::fingerprinter`].
 ///
-/// The fingerprint value is a function of the `Params`, the seed, and
-/// of the bytes written to the `Fingerprinter`, but independent of
-/// the size of the individual slices written to the hasher.
+/// The fingerprint value is a function of the [`Params`], the seed,
+/// and of the bytes written to the [`Fingerprinter`, but independent
+/// of the size of the individual slices written to the hasher.
 ///
 /// In other words, it doesn't matter how we partition an input, the
-/// `Fingerprinter` computes the same fingerprint as a one-shot UMASH
-/// call for the parameters and the concatenated input bytes.
+/// [`Fingerprinter`] computes the same fingerprint as a one-shot
+/// UMASH call for the parameters and the concatenated input bytes.
 ///
 /// The two 64-bit value in the resulting fingerprint are equivalent
 /// to the value computed by the 64-bit UMASH functions for the same
-/// `Params`, seed, and inputs, with `UmashComponent::Hash` for the
-/// `Fingerprint::hash[0]`, and with `UmashComponent::Secondary` for
+/// [`Params`], seed, and inputs, with [`UmashComponent::Hash`] for the
+/// `Fingerprint::hash[0]`, and with [`UmashComponent::Secondary`] for
 /// `Fingerprint::hash[1]`.
 ///
-/// When used as a `std::hash::Hasher`, the hash value computed by a
-/// `Fingerprinter` is equivalent to the `Hasher` for
-/// `UmashComponent::Hash`.
+/// When used as a [`std::hash::Hasher`], the hash value computed by a
+/// [`Fingerprinter`] is equivalent to the [`Hasher`] for
+/// [`UmashComponent::Hash`].  That's not useful in itself, but it
+/// makes sense to pass a [`Fingerprinter`] to a `std::hash::Hash`,
+/// and extract a [`Fingerprint`] for the input data with
+/// [`Fingerprinter::digest`].
 #[derive(Clone)]
 pub struct Fingerprinter<'params>(ffi::umash_fp_state, PhantomData<&'params Params>);
 
 impl Params {
-    /// Returns a new pseudo-unique `Params` value.
+    /// Returns a new pseudo-unique [`Params`] value.
     pub fn new() -> Self {
         use std::cell::Cell;
 
@@ -147,10 +153,10 @@ impl Params {
         })
     }
 
-    /// Returns a fresh set of `Params` derived deterministically from
-    /// `bits` and the first 32 bytes in `key`.
+    /// Returns a fresh set of [`Params`] derived deterministically
+    /// from `bits` and the first 32 bytes in `key`.
     ///
-    /// The umash function defined by the resulting `Params` will
+    /// The UMASH function defined by the resulting [`Params`] will
     /// remain the same for all versions of UMASH and umash-rs.
     pub fn derive(bits: u64, key: &[u8]) -> Self {
         let mut params: Self = unsafe { std::mem::zeroed() };
@@ -167,41 +173,53 @@ impl Params {
         params
     }
 
-    /// Returns a hasher for the primary UMASH function.
+    /// Returns a [`Hasher`] for the primary UMASH function.
+    ///
+    /// The `seed` tweaks the hash value without any proven impact on
+    /// collision rates for different seed values.
     #[inline(always)]
     pub fn hasher(&self, seed: u64) -> Hasher {
         self.component_hasher(seed, UmashComponent::Hash)
     }
 
-    /// Returns a hasher for the secondary UMASH function.
+    /// Returns a [`Hasher`] for the secondary UMASH function.
+    ///
+    /// The `seed` tweaks the hash value without any proven impact on
+    /// collision rates for different seed values.
     #[inline(always)]
     pub fn secondary_hasher(&self, seed: u64) -> Hasher {
         self.component_hasher(seed, UmashComponent::Secondary)
     }
 
-    /// Returns a hasher for the desired UMASH component (primary hash
-    /// or secondary function).
+    /// Returns a [`Hasher`] for the desired UMASH component (primary
+    /// hash or secondary function).
+    ///
+    /// The `seed` tweaks the hash value without any proven impact on
+    /// collision rates for different seed values.
     #[inline(always)]
     pub fn component_hasher(&self, seed: u64, which: UmashComponent) -> Hasher {
         Hasher::with_params(self, seed, which)
     }
 
-    /// Returns a fingerprinter for the UMASH function.
+    /// Returns a [`Fingerprinter`] for the UMASH function.
+    ///
+    /// The `seed` tweaks the hash value without any proven impact on
+    /// collision rates for different seed values.
     #[inline(always)]
     pub fn fingerprinter(&self, seed: u64) -> Fingerprinter {
         Fingerprinter::with_params(self, seed)
     }
 
-    /// Computes the hash value defined by this set of UMASH params
-    /// for `object` and `seed = 0`.
+    /// Computes the [`UmashComponent::Hash`] value defined by this
+    /// set of UMASH params for `object` and `seed = 0`.
     pub fn hash(&self, object: impl std::hash::Hash) -> u64 {
         let mut hasher = self.hasher(0);
         object.hash(&mut hasher);
         hasher.digest()
     }
 
-    /// Computes the secondary hash value defined by this set of UMASH
-    /// params for `object` and `seed = 0`.
+    /// Computes the [`UmashComponent::Secondary`] hash value defined
+    /// by this set of UMASH params for `object` and `seed = 0`.
     pub fn secondary(&self, object: impl std::hash::Hash) -> u64 {
         let mut hasher = self.secondary_hasher(0);
         object.hash(&mut hasher);
@@ -266,8 +284,8 @@ impl<'a> Hasher<'a> {
         self
     }
 
-    /// Returns the 64-bit hash value for the `Hasher`'s params and
-    /// bytes passed to `write` so far.
+    /// Returns the 64-bit hash value for the [`Hasher`]'s [`Params`]
+    /// and the bytes passed to [`Hasher::write`] so far.
     #[inline(always)]
     pub fn digest(&self) -> u64 {
         unsafe { ffi::umash_digest(&self.0) }
@@ -340,7 +358,8 @@ impl<'a> Fingerprinter<'a> {
     }
 
     /// Returns the 128-bit fingerprint value for the
-    /// `Fingerprinter`'s params and bytes passed to `write` so far.
+    /// [`Fingerprinter`]'s [`Params`] and the bytes passed to
+    /// [`Fingerprinter::write`] so far.
     #[inline(always)]
     pub fn digest(&self) -> Fingerprint {
         let fprint = unsafe { ffi::umash_fp_digest(&self.0) };
